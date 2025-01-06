5f54403a2b71d051086584a91ba1807712d90313
invisible-bo <qhrms111@gmail.com>
2025-01-06 22:45:17 +0900

    0106

--- articles/views.py
+++ articles/views.py
@@ -7,3 +7,16 @@ def index(request):
 
 def users(request):
     return render(request, "users.html")
+
+
+def hello(request):
+    name = "보근"
+    tags = ["python", "django", "html", "css"]
+    books = ["해변의 카프카", "어린왕자", "AI"]
+
+    context = {
+        "name": name,
+        "tags": tags,
+        "books": books,
+    }
+    return render(request, "hello.html", context)
